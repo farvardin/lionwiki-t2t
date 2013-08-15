01376e5815d69d824456f4fb2503040c9db03c1c
farvardin <farvardin@users.noreply.github.com>
2013-08-15 22:21:08 +0200

    adding new templates

--- config.php
+++ config.php
@@ -1,10 +1,15 @@
 <?php /* Configuration file for LionWiki. */
-$WIKI_TITLE = 'My new wiki'; // name of the site
+$WIKI_TITLE = 'LionWiki and txt2tags sample'; // name of the site
 
 // SHA1 hash of password. If empty (or commented out), no password is required
-// $PASSWORD = sha1("my_password");
+
+$PASSWORD = sha1("demo");
 
 $TEMPLATE = 'templates/dandelion.html'; // presentation template
+$TEMPLATE = 'templates/fravashyo.html';
+//$TEMPLATE = 'templates/sissou.html';
+$TEMPLATE = 'templates/minimaxing.html';
+// $TEMPLATE = 'templates/minimal.html';
 
 // if true, you need to fill password for reading pages too
 // before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
