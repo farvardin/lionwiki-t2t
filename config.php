c67d145e82eb9d27df97e47bbdd8157b560fd298
farvardin <farvardin@localhost>
2013-10-26 21:48:12 +0200

    updated some templates

--- config.php
+++ config.php
@@ -8,14 +8,14 @@ $Admin["PASSWORD"] = sha1("demo");
 
 $TEMPLATE = 'templates/dandelion.html'; // presentation template
 $TEMPLATE = 'templates/fravashyo.html';
-//$TEMPLATE = 'templates/sissou.html';
-$TEMPLATE = 'templates/blazekiss.html'; 
+$TEMPLATE = 'templates/sissou.html';
+//$TEMPLATE = 'templates/blazekiss.html'; 
 $TEMPLATE = 'templates/minimaxing.html';
+//$TEMPLATE = 'templates/skeleton.html'; // doesn't work well yet
 $TEMPLATE = 'templates/bootstrap.html';
-
 //$TEMPLATE = 'templates/ElectricObsidian/ElectricObsidian.html';
 //$TEMPLATE = 'templates/literature.html';
-// $TEMPLATE = 'templates/minimal.html';
+//$TEMPLATE = 'templates/minimal.html';
 
 // if true, you need to fill password for reading pages too
 // before setting to true, read http://lionwiki.0o.cz/index.php?page=UserGuide%3A+How+to+use+PROTECTED_READ
