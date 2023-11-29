fdd5fbdd5df73f4a760b38da858345ca73917501
farvardin <eforgeot@gmail.com>
2023-11-29 11:36:04 +0100

    adding more files

---
+++ plugins/abcjs/abcjs_dropdown.js
@@ -0,0 +1,22 @@
+
+
+function dropdown() {
+//   document.getElementsByClassName("abctext")[0].style.display="block";
+
+	var elems = document.getElementsByClassName("abctext");
+	
+	for (i = 0 ; i < elems.length; i++) {
+		//elems[i].style.display="block";
+		 if (elems[i].style.display === 'none') {
+			elems[i].style.display = 'block';
+			elems[i].style.color = '#333';
+			elems[i].style.backgroundColor = '#eee';
+			elems[i].style.padding = '20px';
+		} else {
+			elems[i].style.display = 'none';
+  }
+	}
+
+}
+
+	
