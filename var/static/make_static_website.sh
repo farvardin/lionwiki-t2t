6e523c7255fb89901be8322f3c2277c91f4cba4b
farvardin <eforgeot@gmail.com>
2024-02-02 09:31:33 +0100

    maj templates

--- var/static/make_static_website.sh
+++ var/static/make_static_website.sh
@@ -3,7 +3,7 @@
 # #######################
 #
 # this script will generate a static version from your lionwiki website 
-#  (/var/pages) into the output/ folder
+#  (/var/pages or whatever you choose) into the output/ folder
 #
 # <!> It must be run from "your_lionwiki_root_folder/var/static/"
 #
@@ -15,9 +15,26 @@
 #
 #
 
+## Choose your theme:
 THEME=dandelion
 THEME=cafe
+# for mimoza you will need to manually copy templates/fonts/ into output/
+THEME=mimoza
 
+## Choose the subfolder from lionwiki/var/
+#LOCATION=pages
+LOCATION=minisite
+
+
+## Choose index page name
+INDEX=index
+
+## Choose table of content (option 1) or not (option 2)
+#TOC=--toc
+TOC=""
+
+## Choose to use page name as header (1) or not (0) (use your own titles)
+HEADER=0
 
 help() {
 	printf "\nLionwiki static site generator "
@@ -34,35 +51,40 @@ generate_all() {
 	printf "Page List\n\n" > /tmp/lionwikipagelist.t2t
 
 
-	cd ../pages/
-	for FILENAME in *.txt ; do 
-		printf "${FILENAME%.*}" > /tmp/lionwikistatic.t2t
+	cd ../${LOCATION}/
+	for FILENAME in *.txt *.gmi ; do 
+		printf "${FILENAME%.*}\n" > /tmp/lionwikistatic.t2t
+		if [ ${HEADER} == 1 ]; then 
+			printf "${FILENAME%.*}\n" >> /tmp/lionwikistatic.t2t
+			fi
 		printf "\n\n\n" >> /tmp/lionwikistatic.t2t ; \
-		cat "${FILENAME%.*}.txt" >>  /tmp/lionwikistatic.t2t ; \
-		txt2tags -T ../static/${THEME}_static.html -t html --config-file ../static/config_static.t2t  --config-file ../../config.t2t --css-inside  --toc --outfile ../static/output/"${FILENAME%.*}".html /tmp/lionwikistatic.t2t ; \
+		cat "${FILENAME}" >>  /tmp/lionwikistatic.t2t ; \
+		txt2tags -T ../static/${THEME}_static.html -t html --config-file ../static/config_static.t2t  --config-file ../../config.t2t --css-inside  ${TOC} --outfile ../static/output/"${FILENAME%.*}".html /tmp/lionwikistatic.t2t ; \
 
 		printf "* [${FILENAME%.*} ${FILENAME%.*}.html] \n" >> /tmp/lionwikipagelist.t2t
 		done
 
 	cd ../static/
 
-	txt2tags -T ${THEME}_static.html -t html --config-file config_static.t2t  --config-file ../../config.t2t --css-inside  --toc --outfile ./output/pagelist.html /tmp/lionwikipagelist.t2t 
+	txt2tags -T ${THEME}_static.html -t html --config-file config_static.t2t  --config-file ../../config.t2t --css-inside ${TOC} --outfile ./output/pagelist.html /tmp/lionwikipagelist.t2t 
 
 	cp ${THEME}.css ./output/
 
 	mkdir -p ./output/var/upload/
 	cp -fr ../upload/* ./output/var/upload/
+	
+	cp ./output/${INDEX}.html ./output/index.html
 
 	printf "\n\nThe generated website is in output/ \n\n"
 
 }
 
 generate_single() {
-	cd ../pages/
+	cd ../${LOCATION}/
 		printf "${FILE%.*}" > /tmp/lionwikistatic.t2t
 		printf "\n\n\n" >> /tmp/lionwikistatic.t2t 
-		cat "${FILE%.*}.txt" >>  /tmp/lionwikistatic.t2t 
-		txt2tags -T ../static/${THEME}_static.html -t html --config-file ../static/config_static.t2t  --config-file ../../config.t2t --css-inside  --toc --outfile ../static/output/"${FILE%.*}".html /tmp/lionwikistatic.t2t 
+		cat "${FILE}" >>  /tmp/lionwikistatic.t2t 
+		txt2tags -T ../static/${THEME}_static.html -t html --config-file ../static/config_static.t2t  --config-file ../../config.t2t --css-inside  ${TOC} --outfile ../static/output/"${FILE%.*}".html /tmp/lionwikistatic.t2t 
 
 		
 }
@@ -77,15 +99,16 @@ fi
 if [ $# -eq 1 ]
 	then 
 		if [ $1 == 'all' ]
-	  then
+			then
 		generate_all
-	fi
-		if [ -f ../pages/$1 ]
+	 else
+		if [ -f ../${LOCATION}/$1 ]
 		then
 			FILE=$1
 			generate_single
 		else 
 			printf "\n File doesn't exist, please choose another one\n\n"
+		fi
 		fi
 fi
 
